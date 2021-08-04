c06de2989eea9da13c95433d920144fae65a9fef
922645 <922645@LTSA000373.cts.com>
2021-08-04 17:38:18 +0300

    7

--- newGitRepository/src/newGitRepository/TestGit.java
+++ newGitRepository/src/newGitRepository/TestGit.java
@@ -3,9 +3,9 @@ package newGitRepository;
 public class TestGit {
 	
 	public void method2(){
-		System.out.println("method2");
+		System.out.println("method22");
 	}
 	public void method3(){
-		System.out.println("method3");
+		System.out.println("method33");
 	}
 }
